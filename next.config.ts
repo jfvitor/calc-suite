876d18aa180f03c1c9c03e9facf02d6c7053a493
Jacque Vitor <jacquevitor@gmail.com>
2025-09-02 15:20:14 +0100

    Update and rename next.config.ts to next.config.mjs

--- next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from 'next';
-const nextConfig: NextConfig = {
-  output: 'export', // SSG para Cloudflare Pages
-  images: { unoptimized: true },
-  reactStrictMode: true,
-};
-export default nextConfig;
