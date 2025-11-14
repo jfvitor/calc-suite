8f1f282f143d0b73e0e3bd6e4451587267945733
Jacque Vitor <jacquevitor@gmail.com>
2025-11-14 15:48:47 +0000

    Format JsonLd component for better readability

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+
 export function JsonLd({ json }: { json: object }) {
   return (
-    <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(json) }} />
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify(json),
+      }}
+    />
   );
 }
